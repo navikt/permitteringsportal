cb7d8532ec1394ae5945f4f27ab38f3832b1079d
Mattias Lundmark <lundmark.mattias@gmail.com>
2022-02-18 11:01:38 +0100

    Ikke msw i bygg

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './app/App';
 import { worker } from './mocks/browser';
 
-if (true) {
+if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'production') {
   worker.start({
     serviceWorker: {
       url: '/permitteringsportal/mockServiceWorker.js',
